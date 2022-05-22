bf45b5634c299df567525cb3f10a3636a3e07689
n.kosov <Zxcvb`1234567890>
2022-05-22 19:03:31 +0300

    3 commit

---
+++ src/test/kotlin/sbi/risks/kontur/konturnew/KonturNewApplicationTests.kt
@@ -0,0 +1,13 @@
+package sbi.risks.kontur.konturnew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KonturNewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
